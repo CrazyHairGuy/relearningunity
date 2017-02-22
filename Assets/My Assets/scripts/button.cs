a56c9c34df238864a603c2306f7c0c311d2e4ace
Crazy <crazyhairguy11@gmail.com>
2017-02-21 18:03:01 -0600

    working door and button mechanism, and enhanced handling of grabbed objects

---
+++ Assets/My Assets/scripts/button.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour {
+
+	//public Collider buttonTrigger;
+	public GameObject door1;
+	public GameObject door2;
+	public GameObject buttonTop;
+	public GameObject door1Dest;
+	public GameObject door2Dest;
+	public GameObject buttonTopDest;
+	Vector3 door1T;
+	Vector3 door2T;
+	Vector3 buttonT;
+	public float speed = 5.0f;
+	private bool down = false;
+
+	void OnTriggerEnter(Collider collider){
+
+		Debug.Log ("touch");
+
+		down = true;
+	
+	}
+
+	void OnTriggerExit(Collider collider){
+
+		Debug.Log ("untouch");
+
+		down = false;
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		door1T = door1.transform.position;
+		door2T = door2.transform.position;
+		buttonT = buttonTop.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (down) {
+			
+			door1.transform.position = Vector3.Lerp (door1.transform.position, door1Dest.transform.position, Time.deltaTime * speed);
+			door2.transform.position = Vector3.Lerp (door2.transform.position, door2Dest.transform.position, Time.deltaTime * speed);
+			buttonTop.transform.position = Vector3.Lerp (buttonTop.transform.position, buttonTopDest.transform.position, Time.deltaTime * speed);
+
+		} else {
+
+			door1.transform.position = Vector3.Lerp (door1.transform.position, door1T, Time.deltaTime * speed);
+			door2.transform.position = Vector3.Lerp (door2.transform.position, door2T, Time.deltaTime * speed);
+			buttonTop.transform.position = Vector3.Lerp (buttonTop.transform.position, buttonT, Time.deltaTime * speed);
+
+		}
+	}
+}
--- Assets/My Assets/scripts/grab.cs
+++ Assets/My Assets/scripts/grab.cs
@@ -6,7 +6,18 @@ public class grab : MonoBehaviour {
 
 	GameObject grabbedObject;
 	float grabbedObjectSize;
+	//Quaternion lookRot;
+	public float speed = 5.0f;
+	public float rotSpeed = 5.0f;
 	public Vector3 offset = new Vector3(0, 1, 0);
+	Quaternion lookRot;
+	//GameObject[] pickUppable;
+
+	/*Quaternion lookRot(Quaternion cameraRot){
+	
+		return cameraRot.SetLookRotation (Camera.main.transform.position, Vector3.up);
+
+	}*/
 
 	GameObject getMouseHoverObject(float range){
 		
@@ -32,7 +43,7 @@ public class grab : MonoBehaviour {
 
 	bool CanGrab(GameObject candidate){
 
-		return candidate.GetComponent<Rigidbody> () != null;
+		return candidate.CompareTag("grab");
 
 	}
 
@@ -50,6 +61,8 @@ public class grab : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		//pickUppable = GameObject.FindWithTag ("interactable");
+
 		if (Input.GetKeyDown ("e")) {
 			if (grabbedObject == null)
 				TryGrabbedObject (getMouseHoverObject (5));
@@ -57,8 +70,13 @@ public class grab : MonoBehaviour {
 				DropObject ();
 		}
 		if (grabbedObject != null) {
+			//Quaternion lookRot = Quaternion.LookRotation (Camera.main.transform.position, Vector3.up);
+			//Quaternion newRotation = Quaternion.Lerp (grabbedObject.transform.rotation, lookRot, Time.deltaTime * rotSpeed);
+			lookRot.Set(this.transform.rotation[0], Camera.main.transform.rotation[1], this.transform.rotation[2], Camera.main.transform.rotation[3]);
+			grabbedObject.transform.rotation = Quaternion.Lerp (grabbedObject.transform.rotation, lookRot, Time.deltaTime * rotSpeed);
 			Vector3 newPosition = (gameObject.transform.position + Camera.main.transform.forward * grabbedObjectSize) + offset;
-			grabbedObject.transform.position = newPosition;
+			grabbedObject.transform.position = Vector3.Lerp(grabbedObject.transform.position, newPosition, Time.deltaTime * speed);
+			//grabbedObject.transform.rotation = newRotation;
 		}
 	}
 }
